6c4ffcf39c483c3c6eba2ab873225ca395cc08e7
solomon <samysam204o@gmail.com>
2024-10-31 20:27:11 +0300

    nothing much

--- app/(protected)/(drawer)/_layout.tsx
+++ app/(protected)/(drawer)/_layout.tsx
@@ -1,6 +1,6 @@
 import { ThemedView } from "@/components/ThemedView";
-import { Slot } from "expo-router";
+import { Drawer } from "expo-router/drawer";
 
 export default function DrawerLayout() {
-  return <Slot />;
+  return <Drawer />;
 }
